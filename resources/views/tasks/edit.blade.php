d4a5cf11067f97c5b73ae7786d99c9ee6fffaeb0
jiro.kirameki <ec2-user@ip-172-31-40-226.us-east-2.compute.internal>
2018-06-01 03:49:23 +0000

    bootstrap design

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -7,17 +7,17 @@
               
                
     <div class="row">
-        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6>
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
             {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
             
             
                 <div class="form-group">
-                    {!! Form::label('staus', 'ステータス:') !!}
+                    {!! Form::label('status', 'ステータス:') !!}
                     {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
         
                 <div class="form-group">
-                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::label('content', 'タスク:') !!}
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
         
