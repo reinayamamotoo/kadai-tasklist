ee3c0fc5574b2721c32c8b62a52652c2370a28e3
jiro.kirameki <ec2-user@ip-172-31-40-226.us-east-2.compute.internal>
2018-06-07 03:24:33 +0000

    user pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     @if (Auth::check())
-        <?php $user = Auth::user(); ?>
+        <?php  $user = Auth::user(); ?>
         {{ $user->name }}
     @else
         <div class="center jumbotron">
